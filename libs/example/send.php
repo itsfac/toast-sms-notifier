3be7180053d0b99f85c58856b8665a53f8bd3461
itkoo <itkoo2000@gmail.com>
2020-05-06 10:57:32 +0900

    Add comment to send.php

--- libs/example/send.php
+++ libs/example/send.php
@@ -5,13 +5,14 @@ $toastSMS = new ToastSMS("api-key", "sender phone number");
 /* recipient list */
 $recipientList = array();
 
-/* A recipient info, available to set options */
-$recipient = new ToastSMSRecipient("01012345678");
+/* Create array of recipient's information */
+$recipient = new ToastSMSRecipient("01012345678"); // recipient's information, available to set options
 array_push($recipientList, $recipient);
 
 /* Toast SMS Options, required text(message content), available to set other options */
 $smsOption = new ToastSMSOption("text");
 
+/* ------------- SMS ------------- */
 /* Send SMS */
 $toastSMS->sendSMS($recipientList, $smsOption);
 
@@ -27,6 +28,7 @@ $tagExpressionList = array("tag1", "tag2");
 $smsOption->setTagExpression($tagExpressionList);
 $toastSMS->sendTagSMS($recipientList, $smsOption);
 
+/* ------------- MMS ------------- */
 /* Toast MMS Options, required text(message content), title, available to set other options */
 $mmsOption = new ToastSMSOption("text", "title");
 
@@ -37,9 +39,7 @@ $toastSMS->sendMMS($recipientList, $mmsOption);
 $rejectionNumber = "08012345678"; // rejection number (console > 080 rejection setting)
 $toastSMS->sendAdMMS($recipientList, $mmsOption, $rejectionNumber);
 
-/* Send Tag SMS, tag expression array */
+/* Send Tag LMS, tag expression array */
 $tagExpressionList = array("tag1", "tag2");
 $mmsOption->setTagExpression($tagExpressionList);
 $toastSMS->sendTagLMS($recipientList, $mmsOption);
-
-
